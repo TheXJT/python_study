0413125db981d80531e91f90162ab30605aca7ed
TheXJT <1251284167@qq.com>
2015-11-16 12:45:27 +0800

    example of __getattr__

---
+++ ex.__getattr__.py
@@ -0,0 +1,12 @@
+class Student(object):
+    def __init__(self):
+        self.name='xujiatian'
+    def __getattr__(self,attr):
+        if attr=='score':
+           return 99
+        raise AttributeError('\'Student\' object has no attribute \'%s\'' % attr)
+
+s=Student()
+print(s.name)
+print(s.score)
+print(s.age)
