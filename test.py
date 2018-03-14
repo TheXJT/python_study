220ddff2441b3b4e4206a31ba11fa291ebdf5e64
unknown <1251284167@qq.com>
2018-03-14 11:20:18 +0800

    first in asus

--- test.py
+++ test.py
@@ -1,4 +1,23 @@
-import random
+from array import array
+import math
 
-a=random.randrange(3)
-print(a)
+class Vector2d:
+	typecode='d'
+
+	def __init__(self,x,y):
+		self.__x=float(x)
+		self.__y=float(y)
+
+	@property
+	def x(self):
+		return self.__x
+
+	@property
+	def y(self):
+		return self.__y
+
+v1=Vector2d(3,4)
+print(v1.__dict__)
+v1._Vector2d__x=float(5)
+print(v1.y,v1.x)
+print(v1._Vector2d__x)
