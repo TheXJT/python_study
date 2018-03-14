2d8cca5cf71e2646b3b3dbbd7b946893de954a48
unknown <1251284167@qq.com>
2018-03-14 17:00:18 +0800

    page 409

--- test.py
+++ test.py
@@ -2,11 +2,13 @@ from array import array
 import math
 
 class Vector2d:
+	__slots__=('__x','__y')
 	typecode='d'
 
-	def __init__(self,x,y):
+	def __init__(self,x,y,z):
 		self.__x=float(x)
 		self.__y=float(y)
+		# self.__z=float(z)
 
 	@property
 	def x(self):
@@ -16,8 +18,9 @@ class Vector2d:
 	def y(self):
 		return self.__y
 
-v1=Vector2d(3,4)
-print(v1.__dict__)
+v1=Vector2d(3,4,5)
 v1._Vector2d__x=float(5)
 print(v1.y,v1.x)
 print(v1._Vector2d__x)
+# v1.__z=2
+print(v1.x)
